37acd5aa10f4a5837a7803420b5cfbd7e2ca20c2
Oleg <117010302+Jaguar-JS@users.noreply.github.com>
2025-08-26 18:56:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 OPEN_AI_TOKEN = os.getenv("OPENAI_API_KEY")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-if not all([TG_BOT_TOKEN, OPEN_AI_TOKEN, GROQ_API_KEY]):
+if not all([TG_BOT_TOKEN, OPEN_AI_TOKEN]):
+
     raise ValueError("Введите токены в .env")
